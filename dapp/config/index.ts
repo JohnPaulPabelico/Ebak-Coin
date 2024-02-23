53c495e97386f271647a457fd06c0dbdcfd99db5
John Paul Pabelico <89491569+JohnPaulPabelico@users.noreply.github.com>
2024-02-23 21:47:50 +0000

    Updated contract address

--- dapp/config/index.ts
+++ dapp/config/index.ts
@@ -3,7 +3,7 @@ import abi from "./abi.json";
 
 export function getContract(signer: ContractRunner) {
     return new Contract(
-        "0x5874F7d16179C802DCD9f52e63f30Fc84394997A",
+        "0x6309BcFF460e3a610757e3f7f1bEA542FFB76F0c",
         abi as any,
         signer
     );
